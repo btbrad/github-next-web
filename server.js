099727e1a59c8deec1d823ed3e964f47c18ce31f
btbrad <biantaobrad@gmail.com>
2021-02-08 22:36:16 +0800

    添加koa-router

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const next = require('next')
+const KoaRouter = require('koa-router')
 
+const router = new KoaRouter()
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -11,6 +13,8 @@ app.prepare().then(() => {
     await handle(ctx.req, ctx.res)
     ctx.respond = false
   })
+
+  server.use(router.routes())
 
   server.listen(3000, () => {
     console.log('Koa server is running at 3000')
